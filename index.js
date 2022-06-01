d5a6a1de1c26f77d11e152200a2c456763c1a05e
diegocastilloupgrade <diego.castillo@bootcamp-upgrade.com>
2022-06-01 10:34:06 +0200

    estructura 2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//index.js del proyecto
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const logger = require("morgan");
+
+//Importamos los routers
+
+dotenv.config()
+
+const { connect } = require("./src/utils/database");
+connect();
+
+const PORT = process.env.PORT || 5000;
+
+//RECUPERAMOS LA CLAVE SECRETA DEL DOTENV
+const JWT_SECRET = process.env.JWT_SECRET
